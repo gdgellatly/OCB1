6c969a2dd962eb5f6905b754b611d24dd01f4255
Graeme Gellatly <gdgellatly@gmail.com>
2015-01-08 09:50:05 +1300

    [UPD] Port anglosaxon purchase.py

--- addons/account_anglo_saxon/purchase.py
+++ addons/account_anglo_saxon/purchase.py
@@ -19,22 +19,20 @@
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
+from openerp.osv import orm
+from .invoice import get_account
 
-class purchase_order(osv.osv):
-    _name = "purchase.order"
+
+class PurchaseOrder(orm.Model):
     _inherit = "purchase.order"
-    _description = "Purchase Order"
 
     def _choose_account_from_po_line(self, cr, uid, order_line, context=None):
-        account_id = super(purchase_order, self)._choose_account_from_po_line(cr, uid, order_line, context=context)
+        account_id = super(PurchaseOrder, self)._choose_account_from_po_line(
+            cr, uid, order_line, context=context)
         if order_line.product_id and not order_line.product_id.type == 'service':
-            acc_id = order_line.product_id.property_stock_account_input and order_line.product_id.property_stock_account_input.id
-            if not acc_id:
-                acc_id = order_line.product_id.categ_id.property_stock_account_input_categ and order_line.product_id.categ_id.property_stock_account_input_categ.id
-            if acc_id:
-                fpos = order_line.order_id.fiscal_position or False
-                account_id = self.pool.get('account.fiscal.position').map_account(cr, uid, fpos, acc_id)
+            fpos = order_line.order_id.fiscal_position or False
+            account_id = get_account(
+                cr, uid, order_line.product_id,
+                'property_stock_account_input', fpos)
         return account_id
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
