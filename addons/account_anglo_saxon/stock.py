43bc30dadf7825acac55473b685616bd662d2c9d
Graeme Gellatly <gdgellatly@gmail.com>
2015-01-08 09:56:30 +1300

    [UPD] Port stock.py of anglosaxon

--- addons/account_anglo_saxon/stock.py
+++ addons/account_anglo_saxon/stock.py
@@ -19,45 +19,27 @@
 #
 ##############################################################################
 
-from openerp.osv import osv
+from openerp.osv import orm
+from .invoice import get_account
 
-#----------------------------------------------------------
-# Stock Picking
-#----------------------------------------------------------
-class stock_picking(osv.osv):
+
+class StockPicking(orm.Model):
     _inherit = "stock.picking"
-    _description = "Picking List"
 
     def action_invoice_create(self, cr, uid, ids, journal_id=False,
-            group=False, type='out_invoice', context=None):
-        '''Return ids of created invoices for the pickings'''
-        res = super(stock_picking,self).action_invoice_create(cr, uid, ids, journal_id, group, type, context=context)
-        if type == 'in_refund':
-            for inv in self.pool.get('account.invoice').browse(cr, uid, res.values(), context=context):
+                              group=False, type='out_invoice', context=None):
+        """Return ids of created invoices for the pickings"""
+        res = super(StockPicking, self).action_invoice_create(
+            cr, uid, ids, journal_id, group, type, context=context)
+
+        if type in ['in_refund', 'in_invoice']:
+            for inv in self.pool['account.invoice'].browse(
+                    cr, uid, res.values(), context=context):
                 for ol in inv.invoice_line:
                     if ol.product_id:
-                        oa = ol.product_id.property_stock_account_output and ol.product_id.property_stock_account_output.id
-                        if not oa:
-                            oa = ol.product_id.categ_id.property_stock_account_output_categ and ol.product_id.categ_id.property_stock_account_output_categ.id
-                        if oa:
-                            fpos = ol.invoice_id.fiscal_position or False
-                            a = self.pool.get('account.fiscal.position').map_account(cr, uid, fpos, oa)
-                            self.pool.get('account.invoice.line').write(cr, uid, [ol.id], {'account_id': a})
-                            
-        elif type == 'in_invoice':
-            for inv in self.pool.get('account.invoice').browse(cr, uid, res.values(), context=context):
-                for ol in inv.invoice_line:
-                    if ol.product_id:
-                        oa = ol.product_id.property_stock_account_input and ol.product_id.property_stock_account_input.id
-                        if not oa:
-                            oa = ol.product_id.categ_id.property_stock_account_input_categ and ol.product_id.categ_id.property_stock_account_input_categ.id        
-                        if oa:
-                            fpos = ol.invoice_id.fiscal_position or False
-                            a = self.pool.get('account.fiscal.position').map_account(cr, uid, fpos, oa)
-                            self.pool.get('account.invoice.line').write(cr, uid, [ol.id], {'account_id': a})
+                        fpos = ol.invoice_id.fiscal_position or False
+                        account = 'property_stock_account_%s' % ((inv.type == 'in_invoice') and 'input' or 'output')
+                        contra_acc = get_account(cr, uid, ol.product_id, account, fpos)
+                        if contra_acc:
+                            ol.write({'account_id': contra_acc})
         return res
-
-stock_picking()
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
